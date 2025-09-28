bcd9b889e4c5cc94a67c9285829abafdaf3e2112
Daniel Kuettel <dkuettel@gmail.com>
2025-09-28 14:42:17 +0200

    try to make the public interface easier to read

--- lua/funky-formatter.lua
+++ lua/funky-formatter.lua
@@ -1,13 +1,9 @@
-local M = {}
-
---a formatter takes a path and formats it in place
----@alias Formatter fun(path:string):vim.SystemCompleted
-
--- use M.path for arguments in a cmd string[] that are replaced with the file to format
-M.path = 0
+-- NOTE rather inconveniently, refer to the bottom of the file for the public interface
 
 ---@type table<string,Formatter>
-M.config = {}
+local config = {}
+
+local path_token = 0
 
 ---@param cmd (string|0)[]
 ---@param path string
@@ -15,7 +11,7 @@ M.config = {}
 local function run_with_path(cmd, path)
     local call = vim.iter(cmd)
         :map(function(arg)
-            if arg == M.path then
+            if arg == path_token then
                 return path
             end
             return arg
@@ -26,7 +22,7 @@ end
 
 ---@param cmds (string|0)[][]
 ---@return Formatter
-function M.from_cmds(cmds)
+local function from_cmds(cmds)
     return function(path)
         local result
         for _, cmd in ipairs(cmds) do
@@ -41,13 +37,7 @@ end
 
 ---@param cmd (string|0)[]
 ---@return Formatter
-function M.from_cmd(cmd)
-    return M.from_cmds({ cmd })
-end
-
----@param cmd (string|0)[]
----@return Formatter
-function M.from_stdout(cmd)
+local function from_stdout(cmd)
     return function(path)
         local result = run_with_path(cmd, path)
         if result.code ~= 0 then
@@ -60,129 +50,24 @@ function M.from_stdout(cmd)
     end
 end
 
----@type table<string,table<string,Formatter>>
-M.formatters = {
-    python = {
-        ruff = M.from_cmds({
-            { "ruff", "check", "--fix-only", "--select", "I", "--silent", M.path },
-            { "ruff", "format", "--quiet", M.path },
-        }),
-    },
-    lua = {
-        stylua = M.from_cmd({ "stylua", "--search-parent-directories", M.path }),
-    },
-    json = {
-        jq = M.from_stdout({ "jq", ".", M.path }),
-    },
-    yaml = { prettier = M.from_cmd({ "prettier", "--parser", "yaml", M.path }) },
-    html = { prettier = M.from_cmd({ "prettier", "--parser", "html", M.path }) },
-    rust = { rustfmt = M.from_cmd({ "rustfmt", M.path }) },
-    markdown = { pandoc = M.from_stdout({ "pandoc", "--from=markdown", "--to=markdown", M.path }) },
-    gitignore = { sort = M.from_stdout({ "env", "-", "LC_ALL=C", "sort", "--unique", M.path }) },
-    nix = { nixpkgsfmt = M.from_cmd({ "nixpkgs-fmt", M.path }) },
-    toml = { taplo = M.from_cmd({ "taplo", "format", "--option", "indent_string=    ", M.path }) },
+---@type table<string,Formatter>
+local configs = {
+    python_ruff_ruff = from_cmds({
+        { "ruff", "check", "--fix-only", "--select", "I", "--silent", path_token },
+        { "ruff", "format", "--quiet", path_token },
+    }),
+    lua_stylua = from_cmds({ { "stylua", "--search-parent-directories", path_token } }),
+    json_jq = from_stdout({ "jq", ".", path_token }),
+    yaml_prettier = from_cmds({ { "prettier", "--parser", "yaml", path_token } }),
+    html_prettier = from_cmds({ { "prettier", "--parser", "html", path_token } }),
+    rust_rustfmt = from_cmds({ { "rustfmt", path_token } }),
+    markdown_pandoc = from_stdout({ "pandoc", "--from=markdown", "--to=markdown", path_token }),
+    gitignore_sort = from_stdout({ "env", "-", "LC_ALL=C", "sort", "--unique", path_token }),
+    nix_nixpkgsfmt = from_cmds({ { "nixpkgs-fmt", path_token } }),
+    toml_taplo = from_cmds({ { "taplo", "format", "--option", "indent_string=    ", path_token } }),
 }
 
--- opts maps filetypes to functions that take the path and formatters
----@param opts table<string,Formatter>
-function M.setup(opts)
-    M.config = opts or M.config
-    vim.fn.sign_define("FunkyFormatSign", { linehl = "Search", text = "󰛂" })
-end
-
----@type integer
-local last_format_hrtime = 0
-
--- NOTE What is the approach, and why is this so complicated?
--- Approach:
--- - save the buffer to its file
--- - format the file (not the buffer) with the formatter
--- - read the file (in lua, not into the buffer)
--- - compute the diff unformatted -> formatted
--- - apply that diff to the in-memory buffer
--- - force-save that buffer again to the file
--- - add temporary highlights at the diff to flash to the user
--- Why is this so complicated?
--- - most formatters don't have good support to format from stdin
---   - the biggest problem is how they discover configuration files, they need a path for that
---   - this also means we only support formatting for buffers that are backed by a writeable file
--- - we cannot just read the formatted file into the buffer because
---   - it messes with the cursor position
---   - it messes with the lsp, clearing all diagnostics, because it is not an incremental change
---   - by applying a diff in-memory, the changes look incremental to the lsp again
--- - we have to force save in the end, otherwise vim thinks the file has been changed on disk and will complain
-function M.format(buffer)
-    print("󰁫 Getting funky ...")
-
-    -- NOTE some code below cannot deal with a buffer 0, it has to be an actual id
-    buffer = buffer or vim.api.nvim_get_current_buf()
-
-    local filetype = vim.bo.filetype
-    local formatter = M.config[filetype]
-    if not formatter then
-        print(" No funky formatter for filetype '" .. filetype .. "'.")
-        return
-    end
-
-    local path = assert(vim.api.nvim_buf_get_name(buffer))
-    vim.api.nvim_buf_call(buffer, function()
-        vim.cmd([[silent update]]) -- saving to disk
-    end)
-    local unformatted = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
-
-    local result = formatter(path)
-
-    ---@type integer
-    local now = vim.uv.hrtime() ---@diagnostic disable-line: undefined-field
-    local since_seconds = (now - last_format_hrtime) / 1e9
-    last_format_hrtime = now
-
-    if result.code ~= 0 then
-        if since_seconds > 1 then
-            print(" Formatter was not funky, format again quickly to see details.")
-        else
-            print(" Formatter was not funky.")
-            print(result.stdout .. result.stderr)
-        end
-        return
-    end
-
-    local formatted = vim.fn.readfile(path)
-
-    local unformatted_str = table.concat(unformatted, "\n") .. "\n"
-    local formatted_str = table.concat(formatted, "\n") .. "\n"
-    local diff = assert(vim.diff(unformatted_str, formatted_str, { result_type = "indices", algorithm = "minimal" }))
-    assert(type(diff) == "table")
-
-    if #diff == 0 then
-        -- NOTE vim.diff() is not always accurate (see tools.apply_diff() for more details)
-        -- it should be true that `#diff==0 -> no changes are needed`
-        -- but not always the reverse
-        print(" Code was already funky.")
-    else
-        M.apply_diff(buffer, diff, formatted)
-        M.flash_signs_for_diff(diff, buffer)
-        local before_lines, after_lines = M.get_diff_statistics(diff)
-        print(" " .. before_lines .. " lines of crazy code turned into " .. after_lines .. " lines of funky code.")
-    end
-
-    -- double check that indeed the buffer is formatted
-    if true then
-        local actual = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
-        -- NOTE seems like vim buffers cannot be empty like a file (as in {}), but are always at least {""}
-        if #actual == 1 and actual[1] == "" then
-            actual = {}
-        end
-        assert(vim.deep_equal(actual, formatted), "Buffer is not the same as the file after applying the diff. ")
-    end
-
-    -- force save to let vim know we are synced
-    vim.api.nvim_buf_call(buffer, function()
-        vim.cmd([[silent! write!]])
-    end)
-end
-
-function M.flash_signs_for_diff(diff, buffer)
+local function flash_signs_for_diff(diff, buffer)
     -- TODO indent blank lines plugin uses 10k :)
     local priority = 11000
 
@@ -211,7 +96,7 @@ function M.flash_signs_for_diff(diff, buffer)
     end, 500)
 end
 
-function M.get_diff_statistics(diff)
+local function get_diff_statistics(diff)
     local before = 0
     local after = 0
     for i = 1, #diff do
@@ -233,7 +118,7 @@ end
 ---@param buffer integer
 ---@param diff integer[][]
 ---@param target string[]
-function M.apply_diff(buffer, diff, target)
+local function apply_diff(buffer, diff, target)
     -- apply to buffer the diff to turn it into target
 
     -- the problem with vim.diff(source, target, ...)'s output:
@@ -286,4 +171,148 @@ function M.apply_diff(buffer, diff, target)
     end
 end
 
-return M
+---@type integer
+local last_format_hrtime = 0
+
+-- NOTE What is the approach, and why is this so complicated?
+-- Approach:
+-- - save the buffer to its file
+-- - format the file (not the buffer) with the formatter
+-- - read the file (in lua, not into the buffer)
+-- - compute the diff unformatted -> formatted
+-- - apply that diff to the in-memory buffer
+-- - force-save that buffer again to the file
+-- - add temporary highlights at the diff to flash to the user
+-- Why is this so complicated?
+-- - most formatters don't have good support to format from stdin
+--   - the biggest problem is how they discover configuration files, they need a path for that
+--   - this also means we only support formatting for buffers that are backed by a writeable file
+-- - we cannot just read the formatted file into the buffer because
+--   - it messes with the cursor position
+--   - it messes with the lsp, clearing all diagnostics, because it is not an incremental change
+--   - by applying a diff in-memory, the changes look incremental to the lsp again
+-- - we have to force save in the end, otherwise vim thinks the file has been changed on disk and will complain
+---@param buffer? integer current buffer by default
+local function format(buffer)
+    print("󰁫 Getting funky ...")
+
+    -- NOTE some code below cannot deal with a buffer 0, it has to be an actual id
+    buffer = buffer or vim.api.nvim_get_current_buf()
+
+    local filetype = vim.bo.filetype
+    local formatter = config[filetype]
+    if not formatter then
+        print(" No funky formatter for filetype '" .. filetype .. "'.")
+        return
+    end
+
+    local path = assert(vim.api.nvim_buf_get_name(buffer))
+    vim.api.nvim_buf_call(buffer, function()
+        vim.cmd([[silent update]]) -- saving to disk
+    end)
+    local unformatted = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
+
+    local result = formatter(path)
+
+    ---@type integer
+    local now = vim.uv.hrtime() ---@diagnostic disable-line: undefined-field
+    local since_seconds = (now - last_format_hrtime) / 1e9
+    last_format_hrtime = now
+
+    if result.code ~= 0 then
+        if since_seconds > 1 then
+            print(" Formatter was not funky, format again quickly to see details.")
+        else
+            print(" Formatter was not funky.")
+            print(result.stdout .. result.stderr)
+        end
+        return
+    end
+
+    local formatted = vim.fn.readfile(path)
+
+    local unformatted_str = table.concat(unformatted, "\n") .. "\n"
+    local formatted_str = table.concat(formatted, "\n") .. "\n"
+    local diff = assert(vim.diff(unformatted_str, formatted_str, { result_type = "indices", algorithm = "minimal" }))
+    assert(type(diff) == "table")
+
+    if #diff == 0 then
+        -- NOTE vim.diff() is not always accurate (see tools.apply_diff() for more details)
+        -- it should be true that `#diff==0 -> no changes are needed`
+        -- but not always the reverse
+        print(" Code was already funky.")
+    else
+        apply_diff(buffer, diff, formatted)
+        flash_signs_for_diff(diff, buffer)
+        local before_lines, after_lines = get_diff_statistics(diff)
+        print(" " .. before_lines .. " lines of crazy code turned into " .. after_lines .. " lines of funky code.")
+    end
+
+    -- double check that indeed the buffer is formatted
+    if true then
+        local actual = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
+        -- NOTE seems like vim buffers cannot be empty like a file (as in {}), but are always at least {""}
+        if #actual == 1 and actual[1] == "" then
+            actual = {}
+        end
+        assert(vim.deep_equal(actual, formatted), "Buffer is not the same as the file after applying the diff. ")
+    end
+
+    -- force save to let vim know we are synced
+    vim.api.nvim_buf_call(buffer, function()
+        vim.cmd([[silent! write!]])
+    end)
+end
+
+--- public interface
+return {
+    --a formatter takes a path and formats it in place
+    ---@alias Formatter fun(path:string):vim.SystemCompleted
+
+    --- setup funky formatter
+    --- note that there are no default formatters enabled
+    --- if you dont pass any formatters here, nothing is happening
+    ---@param opts? table<string,Formatter> maps a filetype to the formatter to be used
+    setup = function(opts)
+        config = opts or {}
+        vim.fn.sign_define("FunkyFormatSign", { linehl = "Search", text = "󰛂" })
+    end,
+
+    --- apply the filetype's formatter
+    ---@param buffer? integer current buffer by default
+    format = function(buffer)
+        format(buffer)
+    end,
+
+    --- formatter presets, use from_* to make your own
+    ---@type table<string,Formatter> indexed by {filetype}_{formatter}, as in "lua_stylua"
+    configs = configs,
+
+    --- use with from_cmd and co, place it in the array (string|0)[] where you want the target file to be inserted
+    path = path_token,
+
+    --- create a formatter from a single command
+    ---@param cmd (string|0)[]
+    ---@return Formatter
+    from_cmd = function(cmd)
+        return from_cmds({ cmd })
+    end,
+
+    --- create a formatter from multiple commands that are run in sequence
+    --- they are not piped to each other
+    --- execution is stopped as soon as one fails
+    ---@param cmds (string|0)[][]
+    ---@return Formatter
+    from_cmds = function(cmds)
+        return from_cmds(cmds)
+    end,
+
+    --- create a formatter that uses the stdout of a command to format the file
+    --- the file does not come from stdin
+    --- if the command fails, the target file is left untouched
+    ---@param cmd (string|0)[]
+    ---@return Formatter
+    from_stdout = function(cmd)
+        return from_stdout(cmd)
+    end,
+}
